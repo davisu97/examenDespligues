a952c7fd6f2f04b5d74b7abdd8904a1616cdcc80
david <david@mail.fake>
2023-03-02 11:21:12 +0000

    v2

--- nombreJava26.java
+++ nombreJava26.java
@@ -1,5 +1,7 @@
 public class nombreJava26{
 	public static void main(String[] args){
 	System.out.println("Me llamo David S y he creado el servidor ubuntuserver26");
+	System.out.println("Y vivo en Madrid, España, Europa, Hemisferio Norte");
+
 }
 }
